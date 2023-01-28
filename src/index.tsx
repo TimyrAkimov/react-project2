12e4905a25aeaeec213f15c8ce3f2de80baaea3e
TimyrAkimov <timyrakimov03@gmail.com>
2023-01-28 17:09:03 +0300

    надежда - она у все одна

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -10,5 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
+reportWebVitals()
 
 
