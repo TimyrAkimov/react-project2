da2fa6e62dfc483eae261a3110e2a6f8737e8f45
TimyrAkimov <78687130+TimyrAkimov@users.noreply.github.com>
2023-01-28 03:59:30 +0300

    Add files via upload

---
+++ static/js/CardGrid.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import Card from "./Card";
+import { CardObj } from "./Game";
+
+type CardGridProps = {
+  randomCards: CardObj[] | null;
+  handleClick: (cardIndex: number, card: CardObj) => void;
+};
+
+const CardGrid = ({ randomCards, handleClick }: CardGridProps): JSX.Element => {
+  return (
+    <Wrapper>
+      {randomCards &&
+        randomCards.map((card, index) => (
+          <Card
+            handleClick={handleClick}
+            card={card}
+            cardIndex={index}
+            flippedImage={card.image}
+            key={card.id}
+          />
+        ))}
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  justify-items: center;
+  row-gap: 1rem;
+  column-gap: 1rem;
+  margin: 0 auto;
+`;
+export default CardGrid;
---
+++ static/js/PlayerInfo.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+type PlayerInfoProps = {
+  turns: number;
+  score: number;
+};
+
+const PlayerInfo = ({ turns, score }: PlayerInfoProps) => {
+  return (
+    <Wrapper>
+      <p>
+        {`Turns: `}
+        <strong>{`${turns}`}</strong>
+      </p>
+      <p>
+        {`Pairs found: `}
+        <strong>{`${score}`}</strong>
+      </p>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+  margin-top: auto;
+  font-size: 20px;
+`;
+
+export default PlayerInfo;
