d36516f567375677daaa95600067cac9af3726f8
TimyrAkimov <78687130+TimyrAkimov@users.noreply.github.com>
2023-01-28 03:11:35 +0300

    Add files via upload

---
+++ static/js/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Game from "../components/Game"
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+     <Header />
+     <Game />
+    </>
+  );
+};
+
+export default Home;
