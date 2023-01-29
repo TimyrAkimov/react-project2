42d5aa1f86834c175b65f543fc61cb4b45d6da9b
TimyrAkimov <78687130+TimyrAkimov@users.noreply.github.com>
2023-01-29 22:58:16 +0300

    Update App.tsx

--- static/js/components/App.tsx
+++ static/js/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 
 const App = (): JSX.Element => {
