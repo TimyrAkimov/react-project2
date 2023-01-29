24a015ebbff1f32d35d9178e8c64f4ef4f8f156e
TimyrAkimov <78687130+TimyrAkimov@users.noreply.github.com>
2023-01-29 22:58:38 +0300

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '/react-project2/static/css/index.css';
-import App from '/react-project2/static/js/components/App.tsx';
+import App from '/react-project2/static/js/components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
